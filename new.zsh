7c0f67f200bc12c6cbe13286d1fcb81bb2583f19
mkut <m.k.u.t.at@gmail.com>
2011-10-20 11:55:07 +0900

    new script fix

--- new.zsh
+++ new.zsh
@@ -1,37 +1,14 @@
 #!/bin/zsh
 if [ "$2" = "cpp" ]
 then
-	cp -r ${SP_PATH}/default \!$1
-	cat > \!$1/c++.cpp << EOS
-#include <milib/template/aoj>
-
-class solver
-{
-private:
-public:
-	//>>[solver
-	//>>]
-	//<<[solver
-	//<<]
-	void solve()
-	{
-	}
-};
-
-int main()
-{
-	solver prob;
-	while(cin >> prob)
-	{
-		prob.solve();
-		cout << prob;
-	}
-	return 0;
-}
-EOS
+	mkdir -p \!$1
+	cd \!$1
+	template cf.cpp
 elif [ "$2" = "hs" ]
 then
-	cp -r ${SP_PATH}/default.hs \!$1
+	mkdir -p \!$1
+	cd \!$1
+	template cf.hs
 else
 	echo "Choose language(cpp/hs)."
 fi
