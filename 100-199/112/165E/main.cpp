b14420459f4f295acc4e6b1523b08ab0a9711f06
mkut <m.k.u.t.at@gmail.com>
2012-04-09 10:23:46 +0900

    165E

---
+++ 100-199/112/165E/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class solver {
+	vector<int> a;
+	vector<int> ans;
+public:
+	solver(vector<int>& a) : a(a), ans(1<<22, -1) {
+		for (int i = 0; i < a.size(); i++) {
+			add(((1<<22)-1) ^ a[i], a[i]);
+		}
+	}
+	void solve() {
+		for (int i = 0; i < a.size(); i++) {
+			cout << (i > 0 ? " " : "") << ans[a[i]];
+		}
+		cout << endl;
+	}
+private:
+	void add(int x, int y) {
+		if (ans[x] != -1) {
+			return;
+		}
+		ans[x] = y;
+		for (int i = 0; i < 22; i++) {
+			if ((x >> i) & 1) {
+				add(x ^ (1<<i), y);
+			}
+		}
+	}
+};
+
+int main() {
+	int n; cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	solver prob(a);
+	prob.solve();
+
+	return 0;
+}
