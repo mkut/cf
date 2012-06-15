06718aef40b84ab613d97abadc8b878725753bf4
mkut <m.k.u.t.at@gmail.com>
2012-06-16 07:20:59 +0900

    fin script upd

--- fin.zsh
+++ fin.zsh
@@ -1,15 +1,5 @@
 #!/bin/zsh
 mkdir -p $2
-if [ $# -eq 2 ]
-then
-	mv $1 $2
-else
-	mv $1 $2/$1
-fi
-git add $2/*$1
-if [ $# -eq 2 ]
-then
-	git commit -m "$1"
-else
-	git commit -m "$1 $3"
-fi
+mv $1 $2
+git add $2/$1
+git commit -m "$1 $3"
