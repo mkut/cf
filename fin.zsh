1a847fa2457d63733025a106572287b170eb40bd
mkut <m.k.u.t.at@gmail.com>
2012-06-16 06:38:01 +0900

    new/fin script upd

--- fin.zsh
+++ fin.zsh
@@ -2,9 +2,14 @@
 mkdir -p $2
 if [ $# -eq 2 ]
 then
-	mv *$1 $2/$1
+	mv $1 $2
 else
-	mv *$1 $2
+	mv $1 $2/\!$1
 fi
 git add $2/*$1
-git commit -m "$1"
+if [ $# -eq 2 ]
+then
+	git commit -m "$1"
+else
+	git commit -m "$1 $3"
+fi
--- new.zsh
+++ new.zsh
@@ -1,18 +1,18 @@
 #!/bin/zsh
 if [ "$2" = "cpp" ]
 then
-	mkdir -p \!$1
-	cd \!$1
+	mkdir -p $1
+	cd $1
 	template cf.cpp
 elif [ "$2" = "hs" ]
 then
-	mkdir -p \!$1
-	cd \!$1
+	mkdir -p $1
+	cd $1
 	template cf.hs
 elif [ "$2" = "scala" ]
 then
-	mkdir -p \!$1
-	cd \!$1
+	mkdir -p $1
+	cd $1
 	template cf.scala
 else
 	echo "Choose language(cpp/hs/scala)."
