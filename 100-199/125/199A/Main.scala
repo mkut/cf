b46fbe72afdca2f0e55fa5c95f68ccb3f70684a0
mkut <m.k.u.t.at@gmail.com>
2012-06-24 03:09:38 +0900

    199A

---
+++ 100-199/125/199A/Main.scala
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+object Main {
+
+	def main(args: Array[String]): Unit = {
+		val scanner = new Scanner (System.in)
+		val n = scanner nextInt ()
+		print ("0 0 ")
+		println (n)
+	}
+
+}
